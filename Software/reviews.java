a5f9a4a201fc0058bc32d2fd81591a289dc4cfd0
Telle Abbott <57302004+UnknownTelle@users.noreply.github.com>
2021-05-28 12:06:45 +0100

    Addinf the reviews class
    This class gose throw the reviews attribute allowing them to be but into an array

---
+++ Software/reviews.java
@@ -0,0 +1,15 @@
+package com;
+
+public class reviews {
+    String getName;
+    String getDate;
+    String getRating;
+    String getComments;
+
+    public reviews(String getName, String getDate, String getRating, String getComments){
+        this.getName = getName;
+        this.getDate = getDate;
+        this.getRating = getRating;
+        this.getComments = getComments;
+    }
+}

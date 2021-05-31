e520cee5b691f57cca41d2119091aa936c94859e
Telle Abbott <57302004+UnknownTelle@users.noreply.github.com>
2021-05-31 11:37:39 +0100

    spelling error change
    Changed the method name to correct the spelling error

--- Software/DOHMNScore.java
+++ Software/DOHMNScore.java
@@ -3,7 +3,7 @@ package com;
 import java.util.Arrays;
 
 public class DOHMNScore {
-    public static Restaurant[] OrdedDOHMNScore(Restaurant[] array, String neighbourhood) {
+    public static Restaurant[] OrderDOHMNScore(Restaurant[] array, String neighbourhood) {
         Restaurant[] returnData = new Restaurant[0];
         int bestDOHMHScore = 0;
         for (int i = 0; i <= array.length - 1; i++) {

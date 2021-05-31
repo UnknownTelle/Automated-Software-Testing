838cf6391bf28222b10288dbd721d4456b16e0bc
Telle Abbott <57302004+UnknownTelle@users.noreply.github.com>
2021-05-31 10:55:48 +0100

    Name changes
    change the names to make the class less confussing

---
+++ Software/Reviews.java
@@ -0,0 +1,15 @@
+package com;
+
+public class Reviews {
+    String reviewName;
+    String reviewDate;
+    String reviewRating;
+    String reviewComments;
+
+    public Reviews(String name, String date, String rating, String comments){
+        this.reviewName = name;
+        this.reviewDate = date;
+        this.reviewRating = rating;
+        this.reviewComments = comments;
+    }
+}

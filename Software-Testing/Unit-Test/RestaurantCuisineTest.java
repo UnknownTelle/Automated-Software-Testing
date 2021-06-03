2edb1ab8d282bbd0fcd44267bf2525721c0fcc4b
Telle Abbott <57302004+UnknownTelle@users.noreply.github.com>
2021-06-03 15:48:51 +0100

    RestaurantCuisineTest
    Unit tests for RestaurantCuisine class. it test true, part true and false inputs using the Mock object

---
+++ Software-Testing/Unit-Test/RestaurantCuisineTest.java
@@ -0,0 +1,43 @@
+package com.UnitTest;
+
+import com.Mock.ReadJSONMock;
+import com.Restaurant;
+import com.RestaurantCuisine;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class RestaurantCuisineTest {
+    Restaurant[] restaurants;
+    @BeforeEach
+    public void startUp() throws Exception {
+        restaurants = ReadJSONMock.convertJson(ReadJSONMock.getAPI());
+    }
+    @Test
+    public void testRestaurantCuisineAllTrue(){  // Correct Inputs
+        Restaurant[] restaurantsWithAsianCuisineInManhattan = new Restaurant[2];
+        restaurantsWithAsianCuisineInManhattan[0] = restaurants[0];
+        restaurantsWithAsianCuisineInManhattan[1] = restaurants[2];
+        restaurants = RestaurantCuisine.getRestaurantWithCuisineFromNeighbourhood(restaurants, "Asian", "Manhattan");
+        assertArrayEquals(restaurantsWithAsianCuisineInManhattan, restaurants);
+    }
+    @Test
+    public void testRestaurantCuisineFalseCuisine(){
+        Restaurant[] restaurantsWithFalseCuisineInManhattan = new Restaurant[0];
+        restaurants = RestaurantCuisine.getRestaurantWithCuisineFromNeighbourhood(restaurants, "False", "Manhattan");
+        assertArrayEquals(restaurantsWithFalseCuisineInManhattan, restaurants);
+    }
+    @Test
+    public void testRestaurantCuisineFalseNeighbourhood(){
+        Restaurant[] restaurantsWithAsianCuisineWithFalseNeighbourhood = new Restaurant[0];
+        restaurants = RestaurantCuisine.getRestaurantWithCuisineFromNeighbourhood(restaurants, "Asian", "False");
+        assertArrayEquals(restaurantsWithAsianCuisineWithFalseNeighbourhood, restaurants);
+    }
+    @Test
+    public void testRestaurantCuisineAllFalse(){
+        Restaurant[] restaurantsWithFalseCuisineWithFalseNeighbourhood = new Restaurant[0];
+        restaurants = RestaurantCuisine.getRestaurantWithCuisineFromNeighbourhood(restaurants, "False", "False");
+        assertArrayEquals(restaurantsWithFalseCuisineWithFalseNeighbourhood, restaurants);
+    }
+}

a1f9acaf20214bcbeaa4e2ed2e279a1983c74827
Telle Abbott <57302004+UnknownTelle@users.noreply.github.com>
2021-06-03 19:34:59 +0100

    Name chages
    canged some of the naming

--- Software-Testing/Unit-Test/ReadJSONTest.java
+++ Software-Testing/Unit-Test/ReadJSONTest.java
@@ -1,6 +1,4 @@
 package com.UnitTest;
-
-
 import com.Mock.ReadJSONMock;
 import com.ReadJSON;
 import org.junit.jupiter.api.Test;
@@ -11,10 +9,9 @@ import java.io.IOException;
 public class ReadJSONTest {
 
     @Test
-    public void getAPIIfFileReadsInCorrectly() throws IOException { //name it better
+    public void testReadJSONIsReadingFileCorrectly() throws IOException {
         String mockFile = ReadJSONMock.getAPI();
-        String onlineFile = ReadJSON.getAPI("restaurant-data.json");
-        assertEquals(mockFile, onlineFile);
+        String originalFile = ReadJSON.getAPI("restaurant-data.json");
+        assertEquals(mockFile, originalFile);
     }
-
 }

68c8a2b69e8a90656be59a68185f15854db487b1
Telle Abbott <57302004+UnknownTelle@users.noreply.github.com>
2021-06-03 14:37:46 +0100

    Changed getDistance method
    This method will now work out the distance of all restaurants and then place the ones within the neighbourhood given to the returnData array. This method will then sort that array so it displays the closer restaurants first and the futher way last

--- Software/RestaurantLocation.java
+++ Software/RestaurantLocation.java
@@ -1,17 +1,24 @@
 package com;
-
-
 import java.util.Arrays;
+import java.util.*;
 
 public class RestaurantLocation {
-    public static Restaurant[] getLocation(Restaurant[] array, double lat, double lng, String neighbourhood) {
+    public static Restaurant[] getDistance(Restaurant[] array, double hLat, double hLng, String neighbourhood) {
         Restaurant[] returnData = new Restaurant[0];
-        for (int i = 0; i <= array.length - 1; i++) {
-            if (array[i].neighbourhood.equals(neighbourhood) && (lat < array[i].lat + 0.06 && lat > array[i].lat - 0.06) && (lng < array[i].lng + 0.06 && lng > array[i].lng - 0.06)) {
+        double rLat;
+        double rLng;
+        double distance;
+        for (int i = 0; i < array.length - 1; i++){
+            rLat = array[i].LatLng[0];
+            rLng = array[i].LatLng[1];
+            distance = Math.sqrt((hLat - rLat) * (hLat - rLat) + (hLng - rLng) * (hLng - rLng));
+            array[i].distance = distance;
+            if (array[i].neighbourhood.equals(neighbourhood)){
                 returnData = Arrays.copyOf(returnData, returnData.length + 1);
                 returnData[returnData.length - 1] = array[i];
             }
         }
+        Arrays.sort(returnData, Comparator.comparing(Restaurant::distance));
         return returnData;
     }
 

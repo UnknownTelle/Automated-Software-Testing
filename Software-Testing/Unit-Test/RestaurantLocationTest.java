7ac14ce65f59d70b02b943b51d20ee5493dbcb70
Telle Abbott <57302004+UnknownTelle@users.noreply.github.com>
2021-06-03 15:51:14 +0100

    RestaurantLocation
    Unit tests for RestaurantLocation class. it test true and false inputs using the Mock object

---
+++ Software-Testing/Unit-Test/RestaurantLocationTest.java
@@ -0,0 +1,35 @@
+package com.UnitTest;
+
+import com.Mock.ReadJSONMock;
+import com.Restaurant;
+import com.RestaurantLocation;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class RestaurantLocationTest {
+    Restaurant[] restaurants;
+    @BeforeEach
+    public void startUp() throws Exception {
+        restaurants = ReadJSONMock.convertJson(ReadJSONMock.getAPI());
+    }
+    @Test
+    public void testRestaurantLocationAllTrue() {
+        Restaurant[] restaurantsLocationInManhattan = new Restaurant[5];
+        restaurantsLocationInManhattan[0] = restaurants[2];
+        restaurantsLocationInManhattan[1] = restaurants[6];
+        restaurantsLocationInManhattan[2] = restaurants[3];
+        restaurantsLocationInManhattan[3] = restaurants[7];
+        restaurantsLocationInManhattan[4] = restaurants[0];
+        restaurants = RestaurantLocation.getDistance(restaurants, 40.752831, -73.985748, "Manhattan");
+        assertArrayEquals(restaurantsLocationInManhattan, restaurants);
+    }
+
+    @Test
+    public void testRestaurantLocationFalseNeighbourhood() {
+        Restaurant[] manhattanHotelLatLngButFalseNeighbourhood = new Restaurant[0];
+        restaurants = RestaurantLocation.getDistance(restaurants, 40.752831, -73.985748, "False");
+        assertArrayEquals(manhattanHotelLatLngButFalseNeighbourhood, restaurants);
+    }
+}
